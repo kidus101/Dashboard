97cf6895cdb95e99017400ee6059431d095071c9
kidus <kidusabebe101@gmail.com>
2023-08-31 16:41:21 +0300

    feat(Adding the Area chart component to the left column)

--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import CardItem from './CardItem'
+import AreaChartComponent from './AreaChartComponent'
+import TableComponent from './TableComponent'
 
 const LeftColumn = () => {
   return (
     <div className='w-full flex flex-col justify-between p-2'>
       <div className='flex flex-col lg:flex-row gap-2 w-full'>
-
         <p> <CardItem/> </p>
         <p><CardItem/></p>
         <p><CardItem/></p>
       </div>
+
+      <div className='flex-auto w-full'>
+        <AreaChartComponent/>
+       </div>
+
     </div>
   )
 }
