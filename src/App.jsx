ce9348d5221ea36d72c0d58ddea607bc452e2fad
kidus <kidusabebe101@gmail.com>
2023-08-31 14:43:42 +0300

    feat(Adding the Main Grid Layout)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import Sidebar from "../src/components/Sidebar"
 import Navbar from "../src/components/Navbar"
-
+import LeftColumn from "./components/LeftColumn"
+import RightColumn from "./components/RightColumn"
 
 function App() {
 
   return (
     <main className="flex">
       <Sidebar/>
+
       <div className="flex flex-col flex-1 relative">
         <Navbar/>
-      </div>
+        <div className="grid md:grid-cols-3 grid-cols-1 w-full">
+          <div className="col-span-2"><LeftColumn/> </div>
+          <div className="w-full"><RightColumn/></div>
+
+          </div>
+        </div>
+
     </main>
   )
 }
---
+++ src/components/LeftColumn.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LeftColumn = () => {
+  return (
+    <div className='w-full flex flex-col justify-between p-2'>
+      <div className='flex flex-col lg:flex-row gap-2 w-full'>
+        <p>LeftColumn</p>
+        <p>LeftColumn</p>
+        <p>LeftColumn</p>
+      </div>
+    </div>
+  )
+}
+
+export default LeftColumn
