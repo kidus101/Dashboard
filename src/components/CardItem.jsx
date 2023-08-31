5a3451d1bf76ebe7db40314b79efebdc08bf77fa
kidus <kidusabebe101@gmail.com>
2023-08-31 16:17:33 +0300

    feat(Implemented the card component use in the left side of the Sidebar)

---
+++ src/components/CardItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BadgeDelta, Card, Flex, Metric, Text } from "@tremor/react";
+
+const CardItem = () => {
+  return (
+    <Card className="w-xs" decoration="top" decorationColor="indigo">
+        <Flex justifyContent='between' alignItems='center'>
+              <Text>Sales</Text>  
+              <BadgeDelta deltaType='moderateIncrease'> </BadgeDelta>
+        </Flex>
+
+    <Metric>$ 34,743</Metric>
+  </Card>
+  )
+}
+
+export default CardItem
--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import CardItem from './CardItem'
 
 const LeftColumn = () => {
   return (
     <div className='w-full flex flex-col justify-between p-2'>
       <div className='flex flex-col lg:flex-row gap-2 w-full'>
-        <p>LeftColumn</p>
-        <p>LeftColumn</p>
-        <p>LeftColumn</p>
+
+        <p> <CardItem/> </p>
+        <p><CardItem/></p>
+        <p><CardItem/></p>
       </div>
     </div>
   )
